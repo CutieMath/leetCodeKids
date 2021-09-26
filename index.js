0135192ca5a806bd45129fbbed5a411a08d3a046
CutieMath <cutie.math@protonmail.com>
2021-09-26 22:37:38 +1000

    dynamically add names.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Spin the wheel
+// ========================
+// Method to spin the wheel
+// ========================
 let spinner = document.querySelector("#spinner");
 let btn = document.getElementById("start");
 let number = Math.ceil(Math.random() * 3000);
@@ -8,14 +10,33 @@ btn.onclick = function(){
 }
 
 
-// D3 Pie Chart
+// =============
+// D3 Pie chart. 
+// Doc: https://www.d3-graph-gallery.com/graph/pie_annotation.html
+// ========================
+
+
 // set the dimensions and margins of the graph
 const width = 600,
-    height = 600,
-    margin = 20;
+      height = 600,
+      margin = 20;
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 const radius = Math.min(width, height) / 2 - margin
+
+// set the color scale
+const color = d3.scaleOrdinal()
+    .range(["#FFDFD3", "#E0BBE4", "#957DAD", "#D291BC", "#FEC8D8"])
+
+    
+
+// get customer input as pie element
+var data = [];
+const portion = 10;
+
+const pie = d3.pie()
+.value(function(d) {return d[1]})
+
 
 // append the svg object to the div called 'my_dataviz'
 const svg = d3.select("#spinner")
@@ -25,42 +46,43 @@ const svg = d3.select("#spinner")
   .append("g")
     .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
-// Create dummy data
-const data = {Komo: 20, Tommy: 20, Michael:20, Eve:20, Yuxin:20}
+d3.select("body")
+    .on("keydown", function(e) {
+      if(e.keyCode == 188){
+        var inputString = d3.select("#user-input").node().value;
+        var inputArr = inputString.split(',');
+        var currName = inputArr[inputArr.length - 1]; 
+        data[currName] = 10;
 
-// set the color scale
-const color = d3.scaleOrdinal()
-    .range(["#FFDFD3", "#E0BBE4", "#957DAD", "#D291BC", "#FEC8D8"])
+        // Compute the position of each group on the pie:
+        const data_ready = pie(Object.entries(data))
+        // Now I know that group A goes from 0 degrees to x degrees and so on.
 
-// Compute the position of each group on the pie:
-const pie = d3.pie()
-  .value(function(d) {return d[1]})
-const data_ready = pie(Object.entries(data))
-// Now I know that group A goes from 0 degrees to x degrees and so on.
+        // shape helper to build arcs:
+        const arcGenerator = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
 
-// shape helper to build arcs:
-const arcGenerator = d3.arc()
-  .innerRadius(0)
-  .outerRadius(radius)
+        // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+        svg
+        .selectAll('mySlices')
+        .data(data_ready)
+        .join('path')
+          .attr('d', arcGenerator)
+          .attr('fill', function(d){ return(color(d.data[0])) })
+          .attr("stroke", "white")
+          .style("stroke-width", "2px")
+          // .style("opacity", 0.7)
 
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('mySlices')
-  .data(data_ready)
-  .join('path')
-    .attr('d', arcGenerator)
-    .attr('fill', function(d){ return(color(d.data[0])) })
-    .attr("stroke", "white")
-    .style("stroke-width", "2px")
-    .style("opacity", 0.7)
-
-// Now add the annotation. Use the centroid method to get the best coordinates
-svg
-  .selectAll('mySlices')
-  .data(data_ready)
-  .join('text')
-  .text(function(d){ return d.data[0]})
-  .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
-  .style("text-anchor", "middle")
-  .style("font-size", 25)
-  .style("fill", "white")
+        // Now add the annotation. Use the centroid method to get the best coordinates
+        svg
+        .selectAll('mySlices')
+        .data(data_ready)
+        .join('text')
+        .text(function(d){ return d.data[0]})
+        .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
+        .style("text-anchor", "middle")
+        .style("font-size", 25)
+        .style("fill", "white")
+      }
+    });
