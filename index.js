902c522c96d30b580224c36f14ceced205331fad
CutieMath <cutie.math@protonmail.com>
2021-09-27 21:28:41 +1000

    finish bassic code to add name to the pie dynamically

--- index.js
+++ index.js
@@ -24,19 +24,11 @@ const width = 600,
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 const radius = Math.min(width, height) / 2 - margin
 
-// set the color scale
-const color = d3.scaleOrdinal()
-    .range(["#FFDFD3", "#E0BBE4", "#957DAD", "#D291BC", "#FEC8D8"])
-
-    
-
-// get customer input as pie element
-var data = [];
-const portion = 10;
 
 const pie = d3.pie()
 .value(function(d) {return d[1]})
 
+var local = d3.local();
 
 // append the svg object to the div called 'my_dataviz'
 const svg = d3.select("#spinner")
@@ -46,43 +38,74 @@ const svg = d3.select("#spinner")
   .append("g")
     .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
+
+
+// Create dataset
+const data1 = {a: 9, b: 20}
+var data = [];
+const portion = 10;
 d3.select("body")
-    .on("keydown", function(e) {
-      if(e.keyCode == 188){
+    .on("keydown", function() {
+      if(d3.event.keyCode == 188){
         var inputString = d3.select("#user-input").node().value;
         var inputArr = inputString.split(',');
         var currName = inputArr[inputArr.length - 1]; 
         data[currName] = 10;
-
-        // Compute the position of each group on the pie:
-        const data_ready = pie(Object.entries(data))
-        // Now I know that group A goes from 0 degrees to x degrees and so on.
-
-        // shape helper to build arcs:
-        const arcGenerator = d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius)
-
-        // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-        svg
-        .selectAll('mySlices')
-        .data(data_ready)
-        .join('path')
-          .attr('d', arcGenerator)
-          .attr('fill', function(d){ return(color(d.data[0])) })
-          .attr("stroke", "white")
-          .style("stroke-width", "2px")
-          // .style("opacity", 0.7)
-
-        // Now add the annotation. Use the centroid method to get the best coordinates
-        svg
-        .selectAll('mySlices')
-        .data(data_ready)
-        .join('text')
-        .text(function(d){ return d.data[0]})
-        .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
-        .style("text-anchor", "middle")
-        .style("font-size", 25)
-        .style("fill", "white")
+        // call the function to create the pie
+        update(data);
       }
     });
+
+  // set the color scale
+  const color = d3.scaleOrdinal()
+      .range(["#FFDFD3", "#E0BBE4", "#957DAD", "#D291BC", "#FEC8D8"])
+
+
+  var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius)
+
+
+  function update(data) {
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+      .value(function(d) {return d.value; })
+      .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
+    var data_ready = pie(d3.entries(data))
+  
+    // map to data
+    var u = svg.selectAll("path")
+      .data(data_ready)
+  
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    u
+      .enter()
+      .append('path')
+      .each(function(d){
+        local.set(this, d)
+      })
+      .merge(u)
+      .transition()
+      .duration(1000)
+      .attrTween('d', function(d) {
+        var i = d3.interpolate(local.get(this), d);
+        local.set(this, i(0));
+        return function(t) {
+          return arc(i(t));
+        };
+      })
+      .attr('fill', function(d){ return(color(d.data.key)) })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
+  
+    // remove the group that is not present anymore
+    u
+      .exit()
+      .remove()
+  
+  }
+
+
+  update(data1)
